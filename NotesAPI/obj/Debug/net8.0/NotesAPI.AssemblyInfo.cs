20375e42e7da22a25316f0924fc91a195377ddce
Maheswara Rao Yadavalli <maheswararao.yadavalli@gmail.com>
2026-02-09 17:37:41 -0600

    Adding an API with DynamoDB

--- NotesAPI/obj/Debug/net8.0/NotesAPI.AssemblyInfo.cs
+++ NotesAPI/obj/Debug/net8.0/NotesAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotesAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3ae395688f1146a33bcdd49f453aadfd57ebab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotesAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotesAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
